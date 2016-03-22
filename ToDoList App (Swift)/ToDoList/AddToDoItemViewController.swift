89ea781deaab04e0a7bb61647d769b44b3882025
Monte Thakkar <mthakkar@mail.sfsu.edu>
2016-03-22 12:25:12 -0500

    Tutorial: Storyboards Completed (Swift)

---
+++ ToDoList App (Swift)/ToDoList/AddToDoItemViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddToDoItemViewController.swift
+//  ToDoList
+//
+//  Created by Monte's Pro 13" on 3/22/16.
+//  Copyright © 2016 MonteThakkar. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ToDoList App (Swift)/ToDoList/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  ToDoList
+//
+//  Created by Monte's Pro 13" on 3/22/16.
+//  Copyright © 2016 MonteThakkar. All rights reserved.
+//
+
+import UIKit
+
+class ToDoItem: NSObject {
+    var itemName: NSString?
+    var completed: Bool?
+    var creationDate: NSDate?
+}
